45463f987fa96e16a8500e2fadb7c3745d40b14e
David Sanchez <sanchez.dav90@gmail.com>
2017-10-13 18:36:08 -0500

    Menu structure created

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,15 +8,46 @@ import {
   ScrollView
 } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import IonIcons from 'react-native-vector-icons/Ionicons'
 
 const { width, height } = Dimensions.get('window')
 
 class Menu extends Component {
   render() {
     return(
-      <View />
+      <View style={ styles.menu }>
+        <View style={ styles.avatarContainer }>
+          <View style={ styles.avatarImage }>
+            <Image style={ styles.avatar } source={ require('../images/user.png') } />
+            <Text style={ styles.text }>David</Text>
+          </View>
+          <Icon name="exchange" color="white" size={25} />
+        </View>
+        <ScrollView style={ styles.scrollContainer }>
+          <View style={ styles.textWithIcon }>
+            <View style={ styles.withIcon }>
+              <Icon style={ styles.iconWithText } name="download" color="white" size={28} />
+              <Text style={ styles.text }>My Downloads</Text>
+            </View>
+            <Icon style={ styles.rightIcon } name="angle-right" color="white" size={25} />
+          </View>
+          <View style={ styles.textWithIcon }>
+            <View style={ styles.withIcon }>
+              <IonIcons style={ styles.iconWithText } name="md-checkmark" color="white" size={28} />
+              <Text style={ styles.text }>My List</Text>
+            </View>
+            <Icon style={ styles.rightIcon } name="angle-right" color="white" size={25} />
+          </View>
+        </ScrollView>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  menu: {
+    flex: 1
+  }
+})
 
 export default Menu
