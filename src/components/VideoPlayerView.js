f49d94fe8486b45097660b1f9e0ee5fa2ca09be7
David Sanchez <sanchez.dav90@gmail.com>
2017-10-26 12:55:08 -0500

    Video Player Component added

---
+++ src/components/VideoPlayerView.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {Text, View, StyleSheet} from 'react-native'
+
+class VideoPlayerView extends Component {
+  render() {
+    return(
+      <View />
+    )
+  }
+}
+
+export default VideoPlayerView
